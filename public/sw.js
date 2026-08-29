1695f27705cb588b9ce781fc4938f0e45c0670c1
salecolamar <salecolamar@gmail.com>
2026-08-29 15:12:49 -0300

    Deixa a logo fixa na splash com o codigo rodando (em loop) embaixo dela

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lavajato-1788026701695';
+const CACHE = 'lavajato-1788027128793';
 const APP_SHELL = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
