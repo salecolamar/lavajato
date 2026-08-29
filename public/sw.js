337056a924b57a77cb039107a7645ffbe0e71daa
salecolamar <salecolamar@gmail.com>
2026-08-29 02:26:09 -0300

    Remove mascara e validacao de formato da placa

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lavajato-1787980641628';
+const CACHE = 'lavajato-1787981111586';
 const APP_SHELL = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
